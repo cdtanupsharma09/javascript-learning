2671b9ddfc495c5e450cadd42b6076613fe64f0f
Your Name <your@email.com>
2026-05-10 19:33:46 +0530

    day3

--- day3/operatorsandexpressions.js
+++ day3/operatorsandexpressions.js
@@ -18,3 +18,52 @@
 // /= X=X/Y
 // %= X=X%Y
 // **= X=X**Y
+
+//arethematic operators
+console.log(" arethematic operators in JS")
+let a = 45;
+let b = 4;
+console.log("a + b =",a+b)
+console.log("a - b =",a-b)
+console.log("a / b =",a/b)
+console.log("a * b =",a*b)
+console.log("a ** b =",a**b)
+console.log("a % b =",a%b)
+console.log("a++ =",a++)
+console.log("a-- =",a--)
+console.log("++a =",++a)
+console.log("--a =",--a)
+
+// ASSIGNMENT OPERATOR 
+
+console.log(" assignment operators in JS")
+
+ let assignment = 5 ;
+
+assignment += 10 ;
+console.log(assignment);
+
+
+assignment -= 20 ;
+console.log(assignment);
+
+  
+ assignment *= 30 ;
+ console.log(assignment);
+
+ 
+ assignment /= 40 ;
+ console.log(assignment);
+
+
+assignment %= 50 ;
+console.log(assignment);
+
+ 
+ assignment **= 60 ;
+ console.log(assignment);
+
+ 
+
+ // comparision operator
+
