63401e318d7e91a21e32358f76631bc17bae6088
Your Name <your@email.com>
2026-05-10 20:38:46 +0530

    day3

--- day3/operatorsandexpressions.js
+++ day3/operatorsandexpressions.js
@@ -19,51 +19,71 @@
 // %= X=X%Y
 // **= X=X**Y
 
-//arethematic operators
-console.log(" arethematic operators in JS")
-let a = 45;
-let b = 4;
-console.log("a + b =",a+b)
-console.log("a - b =",a-b)
-console.log("a / b =",a/b)
-console.log("a * b =",a*b)
-console.log("a ** b =",a**b)
-console.log("a % b =",a%b)
-console.log("a++ =",a++)
-console.log("a-- =",a--)
-console.log("++a =",++a)
-console.log("--a =",--a)
+//ARITHEMATIC OPERATORS
+//Arithmetic operators in JavaScript are used to perform mathematical calculations like addition, subtraction, multiplication, etc
+
+// console.log(" arethematic operators in JS")
+// let a = 45;
+// let b = 4;
+// console.log("a + b =",a+b)
+// console.log("a - b =",a-b)
+// console.log("a / b =",a/b)
+// console.log("a * b =",a*b)
+// console.log("a ** b =",a**b)
+// console.log("a % b =",a%b)
+// console.log("a++ =",a++)
+// console.log("a-- =",a--)
+// console.log("++a =",++a)
+// console.log("--a =",--a)
 
 // ASSIGNMENT OPERATOR 
+//Assignment operators are used to assign values to variables
 
-console.log(" assignment operators in JS")
+// console.log(" assignment operators in JS")
 
- let assignment = 5 ;
+//  let assignment = 5 ;
 
-assignment += 10 ;
-console.log(assignment);
+// assignment += 10 ;
+// console.log(assignment);
 
 
-assignment -= 20 ;
-console.log(assignment);
+// assignment -= 20 ;
+// console.log(assignment);
 
   
- assignment *= 30 ;
- console.log(assignment);
+//  assignment *= 30 ;
+//  console.log(assignment);
 
  
- assignment /= 40 ;
- console.log(assignment);
+//  assignment /= 40 ;
+//  console.log(assignment);
 
 
-assignment %= 50 ;
-console.log(assignment);
-
- 
- assignment **= 60 ;
- console.log(assignment);
+// assignment %= 50 ;
+// console.log(assignment);
 
  
+//  assignment **= 60 ;
+//  console.log(assignment);
 
- // comparision operator
+ 
 
+ // comparision operators
+ //Comparison operators compare two values and return: true / false
+
+ //  == equal to 
+//  != not equal 
+//  === equal value and type 
+//  !== not equal value or not equal type 
+//  > greater than
+//  < less than 
+//  >= greater than or equal to
+//  <= less the nor equal to
+//  ? ternary operator
+
+let comp1 = 6;
+let comp2 = 6;
+console.log("comp1 == comp2", comp1 == comp2)
+console.log("comp1 != comp2", comp1 != comp2)
+console.log("comp1 === comp2", comp1 === comp2)
+console.log("comp1 !== comp2", comp1 !== comp2)
