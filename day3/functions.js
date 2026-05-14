d1e1d02c85866126f662339df28d267ec1b63eca
Your Name <your@email.com>
2026-05-14 22:25:50 +0530

    day3

--- day3/functions.js
+++ day3/functions.js
@@ -31,22 +31,134 @@
 // console.log(value);
 
 //ARROW FUNCTION (WAY OF WRITING FUNCTION IN SHORT FORM ) =>
-//sum using arrow function
-function sum(a, b){
-   return a+b;
-}
-const arrowsum = (a, b)=>{
-console.log(a+b);
-};
+
+   //sum using arrow function
+// function sum(a, b){
+//    return a+b;
+// }
+// const arrowsum = (a, b)=>{
+// console.log(a+b);
+// };
+
+// //arrow functon is part of modern day java script.
+
+// //multiplication using arrow function
+// function mul(a, b){
+//    return a*b;
+// }
+// const arrowmul = (a, b)=>{
+//    console.log(a*b);
+// };
+
+// const printhello = () => {
+// console.log("hello");
+
+// };
 
 
-//multiplication using arrow function
-function mul(a, b){
-   return a*b;
-}
-const arrowmul = (a, b)=>{
-   console.log(a*b);
-};
+// QUESTION -- create a function using the "function keyword that takes a string as an arguement & returns the number of vowels in the string".
+// function countvowels(str) {
+//    let count =0;
+//    for(const char of str) {
+//       if (
+//          char === "a"||
+//          char === "e"||
+//          char === "i"||
+//          char === "o"||
+//          char === "u"
+//       ){
+// count++;
+//       }
+//    }
+//    return count;
+// }
 
+// const countvowel = (str) => {
 
+//    let count = 0;
+//    for(const char of str) {
+//       if (
+//          char === "a"||
+//          char === "e"||
+//          char === "i"||
+//          char === "o"||
+//          char === "u"
+//       ) {
+// count++;
+//       }
+//    } 
+// return count ; 
+// };
+
+// FOR EACH LOOP IN ARRAYS FUNCTION.
+// FOR EACH IS A METHOD IN ARRAY FUNCTION.
+// INSIDE JAVASCRIPT FUNCTIONS CAN BE PASSED LIKE A PARAMETER
+// a function which can pass anathor function like an arguement is known as callback function.
+
+// function abc(){
+// console.log("hello");
+// }
+// function myfunc(abc) {
+// return abc;
+// };
+
+// let arr = [1,2,3,4,5];
+// arr.forEach(function printvalue(value){
+// console.log(value);
+// });
+
+// let arr = ["gangtok","sikkim","delhi","pakisthan"];
+
+// arr.forEach((value)=>{
+// console.log(value.toUpperCase());
+// });//EXAMPLE OF CALLBACK
+
+// let arr = ["gangtok","sikkim","delhi","pakisthan"];
+
+// arr.forEach((value, index)=>{
+// console.log(value.toUpperCase(), index);
+// });
+// idx = index define position of an element.
+//foreach method / function is higher order function or callback function is a higher order function in javascript
+
+// QUESTION TO SOLVE
+// FOR A GIVEN ARAY OF NUMBERS, PRINT THE SQUARE OF EACH VALUE USING THE ForEach LOOP.
+
+// let nums = [2,3,4,5,6,7];
+// nums.forEach((num) => {
+// console.log(num*num);//nums**2 -- we can use this to get expected output.
+// });
+
+// MAP METHOD
+// CREATES A NEW ARRAY WITH THE RESULTS OF SOME OPERATION.THE VALUE ITS CALLBACK RETURNS ARE USED TO FORM NEW ARRAY
+//  let nums =[67,52,39];
+//  let newArr = nums.map((value)=>{
+//    return value*value;
+//  } );
+//  console.log(newArr);
+
+//  let calcsqare = (num)=>{
+//    console.log(num*num);
+//  };
+ 
+// FILTER METHOD IN AN ARRAY FUNCTION -- filter out as per the condition.
+// let arr =[1,2,3,4,5,6,7,8,9];
+// let evenArr = arr.filter((val) => {
+//    return val % 2 !== 0;
+// });
+// console.log(evenArr);
+
+//REDUCE METHOD-- perform some operations and reduces the array to a single value. it returns that single value.
+// let arr =[1,2,3,4,5,6,7];
+// let output = arr.reduce((result , current)=> {
+// return result + current;
+// });
+// console.log(output);
+
+//to get output a largest number using reduce method
+// let arr =[1,2,3,4,5,6,7,303,6679];
+// let output = arr.reduce((result , current)=> {
+// return result > current ? result : current;//it is the conditon to get largest output.
+// });
+// console.log(output);
 
